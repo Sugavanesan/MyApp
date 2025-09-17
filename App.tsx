89afab0482aa45aee3fca0e72dcd6c69b1bdf200
Sugavanesan <sugavanesan30@gmail.com>
2025-09-17 21:18:16 +0530

    add app icon splash screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import RouteNavigation from './src/navigators/RouteNavigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { PaperProvider } from 'react-native-paper';
 import { GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+
+  useEffect(() => {
+      SplashScreen.hide();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <PaperProvider>
